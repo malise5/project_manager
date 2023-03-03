17ed857a5b65d67f28453f2eea2cf96c9654d4b4
Halkano Malise <kudezx@gmail.com>
2023-03-04 01:27:44 +0300

    deleted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import ProjectNewForm from "./ProjectNewForm";
+import ProjectList from "./ProjectList";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [theme, setTheme] = useState(true);
+
+  const changeTheme = () => {
+    setTheme(!theme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={theme ? "App" : "App light"}>
+      <Header theme={theme} changeTheme={changeTheme} />
+      <ProjectNewForm />
+      <ProjectList />
     </div>
   );
 }
-
-export default App;
