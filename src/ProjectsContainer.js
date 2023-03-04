eea721b3e7f799778fdf112c1e2ff4ac3f02f07a
Halkano Malise <kudezx@gmail.com>
2023-03-05 00:12:09 +0300

    created new Component

--- src/ProjectsContainer.js
+++ src/ProjectsContainer.js
@@ -1,7 +1,25 @@
-export default function ProjectsContainer() {
+import React from "react";
+import { useState } from "react";
+import ProjectList from "./ProjectList";
+import ProjectNewForm from "./ProjectNewForm";
+
+const ProjectsContainer = () => {
+  const [projects, setProjects] = useState([]);
+
+  const loadProjects = () => {
+    fetch("http://localhost:4000/projects")
+      .then((response) => response.json())
+      .then((data) => {
+        setProjects(data);
+      });
+  };
+
   return (
-    <div className="Footer">
-      <h1>ProjectsContainer</h1>
+    <div>
+      <ProjectNewForm />
+      <ProjectList projects={projects} loadProjects={loadProjects} />
     </div>
   );
-}
+};
+
+export default ProjectsContainer;
