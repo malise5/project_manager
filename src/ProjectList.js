419e4bf3370223fe61b69fecc341c05d88b4702e
Halkano Malise <kudezx@gmail.com>
2023-03-07 10:12:41 +0300

    updated component

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -2,12 +2,8 @@ import { useState } from "react";
 import ProjectCard from "./ProjectCard";
 // import projects from "./projects";
 
-export default function ProjectList({ projects, loadProjects }) {
+export default function ProjectList({ projects }) {
   const [search, setSearch] = useState("");
-
-  const handleClick = () => {
-    loadProjects();
-  };
 
   const hanleChange = (e) => {
     setSearch(e.target.value);
@@ -26,15 +22,29 @@ export default function ProjectList({ projects, loadProjects }) {
 
   return (
     <section>
-      <button onClick={handleClick}>Load Projects</button>
-      <h1> All Project</h1>
+      <h1> All Project </h1>
       <div className="filters">
         <button>All</button>
-        <button>Phase1</button>
-        <button>Phase2</button>
-        <button>Phase3</button>
-        <button>Phase4</button>
-        <button>Phase5</button>
+        <button>
+          Phase1 <br></br>
+          {"(html/css) "}
+        </button>
+        <button>
+          Phase2 <br></br>
+          {"(JavaScript) "}
+        </button>
+        <button>
+          Phase3 <br></br>
+          {"(FrontEnd) "}
+        </button>
+        <button>
+          Phase4 <br></br>
+          {"(BackEnd) "}
+        </button>
+        <button>
+          Phase5 <br></br>
+          {"(FullStack) "}
+        </button>
       </div>
       <input
         type="text"
