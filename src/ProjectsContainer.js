04c570392a16bad596e6d053073be5043a9cde13
Halkano Malise <kudezx@gmail.com>
2023-03-07 10:13:19 +0300

    fixed bug

--- src/ProjectsContainer.js
+++ src/ProjectsContainer.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProjectList from "./ProjectList";
 import ProjectForm from "./ProjectForm";
 
 const ProjectsContainer = () => {
   const [projects, setProjects] = useState([]);
 
-  const loadProjects = () => {
-    fetch("http://localhost:4000/projects")
-      .then((response) => response.json())
-      .then((data) => {
-        setProjects(data);
-      });
-  };
+  useEffect(() => {
+    const loadProjects = () => {
+      fetch("http://localhost:3001/projects")
+        .then((response) => response.json())
+        .then((data) => {
+          setProjects(data);
+        });
+    };
+    loadProjects();
+  });
 
   const onAddProject = (formdata) => {
     setProjects((projects) => [formdata, ...projects]);
@@ -21,7 +24,7 @@ const ProjectsContainer = () => {
   return (
     <div>
       <ProjectForm onAddProject={onAddProject} />
-      <ProjectList projects={projects} loadProjects={loadProjects} />
+      <ProjectList projects={projects} />
     </div>
   );
 };
