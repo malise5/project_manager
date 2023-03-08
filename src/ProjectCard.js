c4b14536a9dba802226354701f58b3371239fe01
Halkano Malise <kudezx@gmail.com>
2023-03-08 13:45:23 +0300

    ✨feat: added a new callback

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -1,13 +1,29 @@
-export default function ProjectCard({ project }) {
+import { FaPencilAlt, FaTrash } from "react-icons/fa";
+import { useState } from "react";
+
+export default function ProjectCard({
+  project,
+  onEditProject,
+  onDeleteProject,
+}) {
   // const {id, name, about, phase, link, image} = project
   // console.log(project);
+  const [clapCount, setClapCount] = useState(0);
+  const handleClap = () => setClapCount((clapCount) => clapCount + 1);
+
+  const handleEditClick = () => {
+    onEditProject(project);
+  };
+  const handleDeleteClick = () => {
+    onDeleteProject(project.id);
+  };
+
   return (
     <li className="card">
       <figure className="image">
         <img src={project.image} alt={project.name} />
-        <button className="claps">
-          <span>👏</span>
-          {0}
+        <button onClick={handleClap} className="claps">
+          👏{clapCount}
         </button>
       </figure>
       <section className="details">
@@ -19,6 +35,14 @@ export default function ProjectCard({ project }) {
       </section>
       <footer className="extra">
         <span className="badge blue"> {`Phase ${project.phase}`} </span>
+        <div className="manage">
+          <button onClick={handleEditClick}>
+            <FaPencilAlt />
+          </button>
+          <button onClick={handleDeleteClick}>
+            <FaTrash />
+          </button>
+        </div>
       </footer>
     </li>
   );
